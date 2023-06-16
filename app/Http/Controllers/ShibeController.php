b246d08d0bfe60a9fbabc31cf159e30b6b40c395
manonvv1 <vlie0073@hz.nl>
2023-06-16 10:46:49 +0200

    api gefixt

--- app/Http/Controllers/ShibeController.php
+++ app/Http/Controllers/ShibeController.php
@@ -8,8 +8,8 @@ class ShibeController extends Controller
 {
     private $base_url = 'http://shibe.online/api/shibes?';
     public function get_shibas($count) {
-        $json = file_get_contents($this->base_url . 'count=' . $count);
-        $shiba = json_decode($json);
-        return view('shiba')->with('shiba', $shiba);
+//        $json = file_get_contents($this->base_url . 'count=' . $count);
+//        $shiba = json_decode($json);
+        return view('shiba');
     }
 }
