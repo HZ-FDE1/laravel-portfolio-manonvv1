2898a20ad0e7b37f43a4167727e83d0e2f4781ed
manonvv1 <vlie0073@hz.nl>
2023-06-16 22:58:24 +0200

    picture weg

--- resources/views/form/index.blade.php
+++ resources/views/form/index.blade.php
@@ -22,24 +22,15 @@
                             @foreach($forms as $form)
                                 <div class="panel">
                                     <a class="panel-block" href="{{ route('forms.show', $form) }}">
-                                        <article class="media">
-                                            <figure class="media-left">
-                                                <p class="image is-128x128">
-                                                    <img src="{{$form->img_url}}" alt="form picture">
-                                                </p>
-                                            </figure>
                                             <div class="media-content">
                                                 <div class="heading">
-                                                    <p>
                                                         <h3>Name:</h3><small>{{ $form->name }}</small>
                                                         <br>
                                                         <h3>Age:</h3><small>{{ $form->age }}</small>
                                                         <br>
                                                         <h3>E-mail:</h3><small>{{ $form->email}}</small>
-                                                    </p>
                                                 </div>
                                             </div>
-                                        </article>
                                     </a>
                                 </div>
                             @endforeach
